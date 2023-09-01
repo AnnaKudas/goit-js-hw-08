418b3e654de8cf1751db33aaf159fd5691cb4b4c
Anna Kudas <nnkudas@gmail.com>
2023-09-01 14:00:49 +0200

    task 1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,38 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { galleryItems } from './gallery-items.js';
+
+function createMarkup(arr) {
+  return arr
+    .map(({ preview, original, description }) =>
+      `<li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+              <img 
+                  class="gallery__image"
+                  src="${preview}" 
+                  data-source="${original}"
+                  width="340"
+                  alt="${description}" />
+          </a>
+       </li>`
+    )
+    .join('');
+}
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  /* options, if needed */
+});
+
+const imageList = document.querySelector('.gallery');
+imageList.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+
+
+lightbox.refresh();
+imageList.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (e.target.tagName === 'IMG') {
+    lightbox.open();
+  }
+});
