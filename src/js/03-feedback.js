5b7862c587ef0c6687c5fc41e1278120e15c4589
Anna Kudas <nnkudas@gmail.com>
2023-09-07 11:53:41 +0200

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,44 +2,20 @@ import throttle from "lodash.throttle";
 import "../css/common.css";
 import "../css/03-feedback.css";
 
-const STORAGE_KEY = "feedback-form-state";
-
 const refs = {
-  form: document.querySelector(".feedback-form"),
+  form: document.querySelector("form"),
   input: document.querySelector("input"),
   textarea: document.querySelector("textarea"),
 };
 
-function populateFormOutput() {
-  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-  if (savedData) {
-    refs.input.value = savedData.email || "";
-    refs.textarea.value = savedData.message || "";
-  }
-}
-
-populateFormOutput();
-
-const formData = {};
-
-refs.form.addEventListener("input", throttle(handlerFormOutput, 500));
+refs.input.addEventListener("input", throttle(handlerFormOutput, 500));
+refs.textarea.addEventListener("input", throttle(handlerFormOutput, 500));
 
 function handlerFormOutput(evt) {
-  formData[evt.target.name] = evt.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  localStorage.setItem(evt.target.name, evt.target.value);
 }
 
 refs.form.addEventListener("submit", (evt) => {
   evt.preventDefault();
-
-  // Clear local storage when the form is submitted
-  localStorage.removeItem(STORAGE_KEY);
-
-  // Print the object with data to the console
-  console.log(formData);
-
-  evt.target.reset();
-
-  refs.input.value = "";
-  refs.textarea.value = "";
+  localStorage.clear();
 });
