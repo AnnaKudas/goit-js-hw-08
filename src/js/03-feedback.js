ee87ed22f9fac82da7f0e48c55490a0538077e10
Anna Kudas <nnkudas@gmail.com>
2023-09-09 14:43:43 +0200

    Deleted Textarea, back to one string localStorage

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,16 +6,41 @@ const refs = {
   form: document.querySelector("form"),
   input: document.querySelector("input"),
   textarea: document.querySelector("textarea"),
-};
+}; 
 
-refs.input.addEventListener("input", throttle(handlerFormOutput, 500));
-refs.textarea.addEventListener("input", throttle(handlerFormOutput, 500));
+try {
+  // Retrieve the stored form state from localStorage
+  const stateJSON = localStorage.getItem("feedback-form-state");
+
+  if (stateJSON) {
+    // Parse the JSON string into a JavaScript object
+    const formState = JSON.parse(stateJSON);
+
+    // Populate the input and textarea fields with the stored values
+    refs.input.value = formState.email;
+    refs.textarea.value = formState.text;
+  }
+} catch (error) {
+  console.error("Error loading form state from localStorage:", error);
+}
+
+refs.form.addEventListener("input", throttle(handlerFormOutput, 500));
 
 function handlerFormOutput(evt) {
-  localStorage.setItem(evt.target.name, evt.target.value);
+  const formState = {
+    email: refs.input.value,
+    text: refs.textarea.value,
+  };
+  localStorage.setItem("feedback-form-state", JSON.stringify(formState));
+}
+
+function clearFields () {
+  refs.input.value = "";
+  refs.textarea.value = "";
 }
 
 refs.form.addEventListener("submit", (evt) => {
   evt.preventDefault();
   localStorage.clear();
+  clearFields();
 });
