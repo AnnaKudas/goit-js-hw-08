14b73bc1afbbbe45f5d2f486ca475c20fd88a740
Anna Kudas <nnkudas@gmail.com>
2023-09-09 15:10:18 +0200

    delete only field, not all localstorage

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,16 +1,16 @@
-import throttle from "lodash.throttle";
-import "../css/common.css";
-import "../css/03-feedback.css";
+import throttle from 'lodash.throttle';
+import '../css/common.css';
+import '../css/03-feedback.css';
 
 const refs = {
-  form: document.querySelector("form"),
-  input: document.querySelector("input"),
-  textarea: document.querySelector("textarea"),
-}; 
+  form: document.querySelector('form'),
+  input: document.querySelector('input'),
+  textarea: document.querySelector('textarea'),
+};
 
 try {
   // Retrieve the stored form state from localStorage
-  const stateJSON = localStorage.getItem("feedback-form-state");
+  const stateJSON = localStorage.getItem('feedback-form-state');
 
   if (stateJSON) {
     // Parse the JSON string into a JavaScript object
@@ -21,26 +21,27 @@ try {
     refs.textarea.value = formState.text;
   }
 } catch (error) {
-  console.error("Error loading form state from localStorage:", error);
+  console.error('Error loading form state from localStorage:', error);
 }
 
-refs.form.addEventListener("input", throttle(handlerFormOutput, 500));
+refs.form.addEventListener('input', throttle(handlerFormOutput, 500));
 
 function handlerFormOutput(evt) {
   const formState = {
     email: refs.input.value,
     text: refs.textarea.value,
   };
-  localStorage.setItem("feedback-form-state", JSON.stringify(formState));
+  localStorage.setItem('feedback-form-state', JSON.stringify(formState));
 }
 
-function clearFields () {
-  refs.input.value = "";
-  refs.textarea.value = "";
+function clearFields() {
+  refs.input.value = '';
+  refs.textarea.value = '';
 }
 
-refs.form.addEventListener("submit", (evt) => {
+refs.form.addEventListener('submit', evt => {
   evt.preventDefault();
-  localStorage.clear();
+  console.log(localStorage.getItem(JSON.stringify('feedback-form-state')));
+  localStorage.removeItem('feedback-form-state');
   clearFields();
 });
